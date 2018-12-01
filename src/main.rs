39564be46f4cf2bd9aaa7355e16b4e72b31a6e77
Pailey Quilts <paileyq@gmail.com>
2018-12-01 16:43:57 -0600

    Add remaining two tests

--- src/main.rs
+++ src/main.rs
@@ -51,5 +51,7 @@ mod tests {
   fn test_first_frequency_reached_twice() {
     assert_eq!(0, first_frequency_reached_twice(&[1, -1]));
     assert_eq!(10, first_frequency_reached_twice(&[3, 3, 4, -2, -4]));
+    assert_eq!(5, first_frequency_reached_twice(&[-6, 3, 8, 5, -6]));
+    assert_eq!(14, first_frequency_reached_twice(&[7, 7, -2, -7, -4]));
   }
 }

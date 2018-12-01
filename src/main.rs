6298507df4f90cc880162ae4a22ceb445cab2cff
Pailey Quilts <paileyq@gmail.com>
2018-12-01 00:32:42 -0600

    Try getting args

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use std::env;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
 
 fn main() -> io::Result<()> {
-  let file = File::open("../input/input99")?;
+  let args = env::args().collect();
+
+  let file = File::open(&args[1])?;
   let reader = BufReader::new(file);
 
   for line in reader.lines() {

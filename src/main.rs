c1f7e2f12d0fa18a90f22e4f035a12e055776008
Pailey Quilts <paileyq@gmail.com>
2018-12-01 17:52:03 -0600

    Move day 1 solution to its own module

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
-use std::collections::HashSet;
-use std::iter::FromIterator;
+
+mod day01;
 
 fn main() -> io::Result<()> {
   let args: Vec<String> = env::args().collect();
@@ -18,50 +18,12 @@ fn main() -> io::Result<()> {
 
   println!(
     "Resulting frequency: {}",
-    resulting_frequency(&freq_changes)
+    day01::resulting_frequency(&freq_changes)
   );
   println!(
     "First frequency reached twice: {}",
-    first_frequency_reached_twice(&freq_changes)
+    day01::first_frequency_reached_twice(&freq_changes)
   );
   Ok(())
 }
 
-fn resulting_frequency(freq_changes: &[i32]) -> i32 {
-  freq_changes.iter().sum()
-}
-
-fn first_frequency_reached_twice(freq_changes: &[i32]) -> i32 {
-  let mut seen: HashSet<i32> = HashSet::from_iter(vec![0]);
-
-  freq_changes.iter()
-    .cycle()
-    .scan(0, |freq, &change| {
-      *freq += change;
-      Some(*freq)
-    })
-    .find(|&freq| seen.replace(freq).is_some())
-    .unwrap()
-}
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn test_resulting_frequency() {
-    assert_eq!(3, resulting_frequency(&[1, -2, 3, 1]));
-    assert_eq!(3, resulting_frequency(&[1, 1, 1]));
-    assert_eq!(0, resulting_frequency(&[1, 1, -2]));
-    assert_eq!(-6, resulting_frequency(&[-1, -2, -3]));
-  }
-
-  #[test]
-  fn test_first_frequency_reached_twice() {
-    assert_eq!(2, first_frequency_reached_twice(&[1, -2, 3, 1]));
-    assert_eq!(0, first_frequency_reached_twice(&[1, -1]));
-    assert_eq!(10, first_frequency_reached_twice(&[3, 3, 4, -2, -4]));
-    assert_eq!(5, first_frequency_reached_twice(&[-6, 3, 8, 5, -6]));
-    assert_eq!(14, first_frequency_reached_twice(&[7, 7, -2, -7, -4]));
-  }
-}

6b6bbfd7d528a77178fbeb9baf794ee3994e8630
Pailey Quilts <paileyq@gmail.com>
2019-02-23 22:31:40 -0600

    Dijkstra's algorithm

--- src/day15.rs
+++ src/day15.rs
@@ -1,4 +1,8 @@
+use std::cmp;
+use std::cmp::Ordering;
 use std::fmt;
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fmt::Display;
 use std::fs::File;
 use std::io::BufReader;
@@ -10,20 +14,20 @@ enum Tile {
   Empty,
   Wall,
   Elf,
-  Goblin
+  Goblin,
 }
 
 impl Tile {
   pub fn from_char(c: char) -> Option<Tile> {
     use self::Tile::*;
 
-    match c {
-      '.' => Some(Empty),
-      '#' => Some(Wall),
-      'E' => Some(Elf),
-      'G' => Some(Goblin),
-       _  => None
-    }
+    Some(match c {
+      '.' => Empty,
+      '#' => Wall,
+      'E' => Elf,
+      'G' => Goblin,
+       _  => return None,
+    })
   }
 
   pub fn to_char(self) -> char {
@@ -33,7 +37,7 @@ impl Tile {
       Empty  => '.',
       Wall   => '#',
       Elf    => 'E',
-      Goblin => 'G'
+      Goblin => 'G',
     }
   }
 }
@@ -41,7 +45,7 @@ impl Tile {
 struct World {
   tiles: Vec<Tile>,
   width: usize,
-  height: usize
+  height: usize,
 }
 
 impl World {
@@ -51,6 +55,57 @@ impl World {
     }
 
     Some(self.tiles[y * self.width + x])
+  }
+
+  pub fn distances_from(&self, from_x: usize, from_y: usize) -> HashMap<(usize, usize), usize> {
+    let mut distances = HashMap::new();
+    let mut unvisited = HashSet::new();
+
+    for tile_x in 0..self.width {
+      for tile_y in 0..self.height {
+        if self.tile(tile_x, tile_y) == Some(Tile::Empty) {
+          unvisited.insert((tile_x, tile_y));
+        }
+      }
+    }
+
+    distances.insert((from_x, from_y), 0);
+
+    let mut current = (from_x, from_y);
+    loop {
+      let next_distance = distances[&current] + 1;
+
+      for (dx, dy) in &[(0, 1), (1, 0), (0, -1), (-1, 0)] {
+        let neighbor = (
+          (current.0 as isize + dx) as usize,
+          (current.1 as isize + dy) as usize,
+        );
+
+        if self.tile(neighbor.0, neighbor.1) == Some(Tile::Empty) {
+          let neighbor_distance = distances.entry(neighbor).or_insert(next_distance);
+          *neighbor_distance = cmp::min(*neighbor_distance, next_distance);
+        }
+      }
+
+      unvisited.remove(&current);
+
+      match unvisited.iter().min_by(|a, b| {
+        // None = infinity!
+        match (distances.get(a), distances.get(b)) {
+          (Some(a), Some(b)) => a.cmp(b),
+          (Some(_), None)    => Ordering::Less,
+          (None, Some(_))    => Ordering::Greater,
+          (None, None)       => Ordering::Equal,
+        }
+      }) {
+        Some(&next) if distances.contains_key(&next) => {
+          current = next;
+        },
+        _ => break,
+      };
+    }
+
+    distances
   }
 }
 
@@ -133,6 +188,63 @@ mod tests {
     assert_eq!(world.tile(0, 5), None);
 
     assert_eq!(map.trim(), format!("{}", world));
+  }
+
+  #[test]
+  fn test_world_distances_from() {
+    let map = "
+#######
+#.E...#
+#.....#
+#...G.#
+#######
+";
+
+    let world: World = map.trim().parse().unwrap();
+
+    let distances: HashMap<(usize, usize), usize> = world.distances_from(4, 2);
+
+    assert_eq!(13, distances.len());
+
+    assert_eq!(4, distances[&(1, 1)]);
+    assert_eq!(2, distances[&(3, 1)]);
+    assert_eq!(1, distances[&(4, 1)]);
+    assert_eq!(2, distances[&(5, 1)]);
+    assert_eq!(3, distances[&(1, 2)]);
+    assert_eq!(2, distances[&(2, 2)]);
+    assert_eq!(1, distances[&(3, 2)]);
+    assert_eq!(0, distances[&(4, 2)]);
+    assert_eq!(1, distances[&(5, 2)]);
+    assert_eq!(4, distances[&(1, 3)]);
+    assert_eq!(3, distances[&(2, 3)]);
+    assert_eq!(2, distances[&(3, 3)]);
+    assert_eq!(2, distances[&(5, 3)]);
+  }
+
+  #[test]
+  fn test_world_distances_from_with_unreachable_tiles() {
+    let map = "
+#######
+#E..G.#
+#...#.#
+#.G.#G#
+#######
+";
+
+    let world: World = map.trim().parse().unwrap();
+
+    let distances: HashMap<(usize, usize), usize> = world.distances_from(1, 1);
+
+    assert_eq!(8, distances.len());
+
+    assert_eq!(0, distances[&(1, 1)]);
+    assert_eq!(1, distances[&(2, 1)]);
+    assert_eq!(2, distances[&(3, 1)]);
+    assert_eq!(1, distances[&(1, 2)]);
+    assert_eq!(2, distances[&(2, 2)]);
+    assert_eq!(3, distances[&(3, 2)]);
+    assert_eq!(2, distances[&(1, 3)]);
+    assert_eq!(4, distances[&(3, 3)]);
   }
 }
 
